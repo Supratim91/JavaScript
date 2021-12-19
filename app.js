e08030a606e1c570687125063c3c2fecac6f6cf0
Supratim Paul <supratim.paul0391@gmail.com>
2021-12-19 20:59:31 +0530

    JavaScript Basic

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('Hello World!');
+let a = 30;
+const c = 2.15;
+a = 20;
+// c = 3;   Type Error
+console.log(a, c);
+const num = 8;  // number
+const str = 'abc'   // string
+const bool1 = true;  // boolean
+const bool2 = false;    // boolean
+const err_null = null;  // Data Type
+const err_undefined = undefined;    // data type
+const obj = {key1:'val1', key2:'val2'}; // object
