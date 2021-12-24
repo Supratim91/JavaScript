5b1874191d6c288b90ca6551ec7a2d6ecdd5904c
Supratim Paul <supratim.paul0391@gmail.com>
2021-12-25 00:25:26 +0530

    string literal

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-const pi = 3.14;
-let radius = 10;
-// math operators - +, -, *, /, **, %
-let result = "";
-result = pi * radius**2;
-// console.log(result);
+const title = "Bestsellers of 2021";
+const author = "Mario";
+const likes = 30;
+// The blog called "Bestsellers of 2021" by Mario has 30 likes
+console.log(`The blog called "${title}" by ${author} has ${likes} likes`);
 
-// shorthands
-let likes = 10;
-likes = likes + 1;
-likes++;
-likes += 1;
-// likes *= 2;
-// likes /= 2;
-console.log(likes);
-
-console.log(5/"xyz");       // NaN
+let html = `
+                <h2>${title}</h2>
+                <p>${author}</p>
+                <span>This blog has ${likes} likes</span>
+           `;
+console.log(html);
