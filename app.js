43e4a193a39252d248555aa2872c57800b94d1cb
Supratim Paul <supratim.paul0391@gmail.com>
2021-12-24 23:06:33 +0530

    String work

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ console.log(fullname.toUpperCase());        // returns uppercase of the string
 console.log(fullname.toLowerCase());        // returns lowercase of the string
 console.log("Found @ in", email + ": ", email.indexOf('@'));       // returns index of the @ of the string
 console.log(`Found @ in ${email}: ${email.indexOf('@')}`);         // returns index of the @ of the string
+
+let mail = 'mario@thenetninja.co.uk';
+let result = "";
+// result = mail.lastIndexOf('n');
+// result = mail.slice(2, 15);
+// result = mail.slice(-7, -4);
+// result = mail.slice(0, 2);
+// result = mail.substring(0, 5);
+// result = mail.replace('m', 'w');
+result = mail.replaceAll('n', 'w');
+console.log(result);
