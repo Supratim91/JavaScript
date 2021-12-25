ae0fcfcd132734c306982c9dd3431e08658e113b
Supratim Paul <supratim.paul0391@gmail.com>
2021-12-25 22:46:20 +0530

    Arrays

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const title = "Bestsellers of 2021";
-const author = "Mario";
-const likes = 30;
-// The blog called "Bestsellers of 2021" by Mario has 30 likes
-console.log(`The blog called "${title}" by ${author} has ${likes} likes`);
-
-let html = `
-                <h2>${title}</h2>
-                <p>${author}</p>
-                <span>This blog has ${likes} likes</span>
-           `;
-console.log(html);
+let ninjas = ['Amit', 'Arif', 'Dev'];        // Array of string
+let ages = [25, 50, 60];      // Array of numbers
+let random = ['Amit', 25, 'Dev', 60];        // Array of mix of string & number
+ninjas[1] = "Raj";       
+console.log(ninjas[1]);       // returns character in that index of the string
+console.log(ages[2]);
+console.log(random.length);
+console.log(ninjas.indexOf('Arif'));         // returns -1 because it not exist
+let res = "";
+res = ninjas.push('Rina');
+res = ninjas.pop();
+let newNinjas = ['Maria', 'Riya'];
+// res = ninjas.concat(['Maria', 'Riya']);
+res = ninjas.concat(newNinjas);
+res = ninjas.splice(2, 1);
+res = ninjas.join(',');
+console.log("res", res);
+console.log("ninjas", ninjas);
