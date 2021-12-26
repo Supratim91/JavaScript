3bc2b623f17ad6bd4e6ae531d0a91ce675983cb5
Supratim Paul <supratim.paul0391@gmail.com>
2021-12-26 23:46:38 +0530

    Work with comparison operators

---
+++ To Learn.js
@@ -0,0 +1,25 @@
+                                                // Topic ------ Overview of JS
+// Arithmetic Operators
+// Assignment Operators
+// Comparison or Relational Operators
+// Logical Operators
+// Conditional or Ternary Operators
+// Bitwise Operators
+// Operator Precedence
+// Selection Statements
+// Loops
+// Jump Statements
+// Popup Boxes
+
+
+                                                // Topic ------ JS Objects
+// Properties of an Object
+// Methods of an Object
+// Standard/Built-in Objects ------ String, RegExp, Boolean, Number, Array, Math, Date
+// String Object ------ Properties, Methods, Wrapper Methods, Two Strings Comparison Operators
+// RegExp Object ------ Patterns Used, Quantifiers, Properties, Methods
+// Boolean Object ------ Properties, Methods
+// Number Object ------ Properties, Methods
+// Array Object ------ Properties, Methods
+// Math Object ------ Properties, Methods
+// Date Object ------ Properties, Methods
--- app.js
+++ app.js
@@ -1,7 +1,59 @@
-// null & undefined
-let age = null;
-console.log(age, age + 3, `The age is ${age}`);        // null
-let arr = ['Car', 'BMW'];
-console.log(arr[2]);          // undefined
-console.log(arr[2] + 3);      // NaN
-console.log(typeof null, typeof undefined);       // returns object and undefined
+console.log(true, false, "true", "false");
+let email = "supratim.paul0391@gmail.com";
+let names = ["Mario", "Ted", "Ken"];
+let result;
+// result = email.includes('@');
+result = names.includes("ken");
+console.log(result);
+
+// comparison operators
+let age = 25;
+console.log(age == 25);       // returns true
+console.log(age == 30);       // returns false
+console.log(age != 30);       // returns true
+console.log(age > 20);        // returns true
+console.log(age < 20);        // returns false
+console.log(age <= 25);       // returns true
+console.log(age >= 25);       // returns true
+console.log(age > 25);        // returns false
+console.log(age < 25);        // returns false
+
+console.log('===========');
+let name = 'shaun';
+console.log(name == 'shaun');       // returns true
+console.log(name == 'Shaun');       // returns false
+console.log(name != 'Shaun');       // returns true
+console.log(name != 'shaun');       // returns false
+console.log(name > 'crystal');      // returns true
+console.log(name > 'Shaun');        // returns true
+console.log(name > 'Crystal');      // returns true
+console.log(name >= 'Shaun');       // returns true
+console.log(name >= 'Crystal');     // returns true
+console.log(name <= 'Shaun');       // returns false
+console.log(name <= 'Crystal');     // returns false
+console.log(name < 'Shaun');        // returns false
+console.log(name < 'Crystal');      // returns false
+
+console.log('===========')
+// Loose comparison or loosely type
+console.log(age == 25);         // returns true
+console.log(age == "25");       // returns true
+console.log(age != 25);         // returns false
+console.log(age != "25");       // returns false
+// Strict comparison or strict type
+console.log(age === 25);         // returns true
+console.log(age === "25");       // returns false
+console.log(age !== 25);         // returns false
+console.log(age !== "25");       // returns true
+
+console.log('==========');
+// Loose comparison or loosely type
+console.log(age == 24);         // returns false
+console.log(age == "24");       // returns false
+console.log(age != 24);         // returns true
+console.log(age != "24");       // returns true
+// Strict comparison or strict type
+console.log(age === 24);         // returns false
+console.log(age === "24");       // returns false
+console.log(age !== 24);         // returns true
+console.log(age !== "24");       // returns true
