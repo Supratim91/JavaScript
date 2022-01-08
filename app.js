43fcd76f3f62252b192e17f115b07e5b90bf2b93
Supratim Paul <supratim.paul0391@gmail.com>
2022-01-08 23:35:16 +0530

    If-ElseIf-Else Statement

--- app.js
+++ app.js
@@ -1,71 +1,31 @@
-// for loop
-/* for (let i = 0; i <= 10; i++) {
-    console.log(i);
-} */
-
-const names = ['Ravi', 'Rahul', 'Amit'];
-/* for (let index = 0; index < names.length; index++)
-{
-    const element = names[index];
-    console.log(index, element);
-} */
-
-// while loop
-/* let i = 8;
-while (i >= 5) {
-    console.log("From 8 to", i);
-    i--;
-} */
-
-/* let index = 0;
-while (index < names.length) {
-    const element = names[index];
-    console.log(index, element);
-    index++;
-} */
-
-// do-while loop
-/* let i = 5;
-do {
-    console.log("value of i is :", i);
-    i++;
-} while (i < 8); */
-
-/* let index = 0;
-do {
-    const element = names[index];
-    console.log(index, element);
-    index++;
-} while (index < names.length); */
-
-// for-each loop
-// names.forEach((element, index) => { console.log(element, index) });
-/* function names(element, index) {
-    console.log(element, index);
-} */
-// names.forEach((element, index) => { console.log(element, index) });
-
-// for-of loop
-/* for (const [index, element] of names.entries()) {
-    console.log(index, element);
-} */
-
-// Default method
-/* for (const iterator of names) {
-    console.log(iterator);
-} */
-
-// for-in loop
-let obj = { a: 1, b: 2, c: 3 };
-for (const key in obj) {
-    if (Object.hasOwnProperty.call(obj, key)) {
-        const element = obj[key];
-        console.log(key, element);
-    }
+const age = 25;
+if (age > 20) {
+    console.log("You are over 20 years old.");
 }
-for (const key in names) {
-    if (Object.hasOwnProperty.call(names, key)) {
-        const element = names[key];
-        console.log(key, element);
-    }
+else {
+    console.log("You are under 20 years old.");
+}
+
+
+const names = ['Rahul', 'Riya', 'Dev', 'Payal'];
+if (names.length > 3) {
+    console.log("Greater than 3");
+}
+else if (names.length < 2) {
+    console.log("Less than 2");
+}
+else {
+    console.log("Numbers of element in the array is : " + names.length);
+}
+
+
+const pwd = "P@ssword";
+if (pwd.length >= 5 && pwd.indexOf('@') > -1) {
+    console.log("Password is strong");
+}
+else if (pwd.length >= 8) {
+    console.log("Password is long enough");
+}
+else {
+    console.log("Password is not long enough");
 }
