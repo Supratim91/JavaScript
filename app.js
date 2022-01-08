c8ccf6d1b04cd4dba0fbb0bae6ee3a2b42d85cb7
Supratim Paul <supratim.paul0391@gmail.com>
2022-01-08 22:33:15 +0530

    Array loop

--- app.js
+++ app.js
@@ -1,19 +1,71 @@
-let score = "100";
-// From string to number conversion
-// score = Number(score);
-score = +score;
-console.log(score + 1);
-console.log(typeof score);
+// for loop
+/* for (let i = 0; i <= 10; i++) {
+    console.log(i);
+} */
 
-let result;
-// result = Number("Hello");          // returns NaN, type 'number'
-// result = String(50);               // returns "50", type 'string'
-// result = `${score}`;
-// result = Boolean(100);             // returns true, type 'boolean
-// result = !!100;
-// result = Boolean(0);              // return false, type 'boolean'
-// result = Boolean('0');               // returns true, type 'boolean'
-// result = Boolean('');             // returns false, type 'boolean'
-// result = Boolean(undefined);        // returns false, type 'boolean'
-result = Boolean(null);             // returns false, type 'boolean'
-console.log(result, typeof result);
+const names = ['Ravi', 'Rahul', 'Amit'];
+/* for (let index = 0; index < names.length; index++)
+{
+    const element = names[index];
+    console.log(index, element);
+} */
+
+// while loop
+/* let i = 8;
+while (i >= 5) {
+    console.log("From 8 to", i);
+    i--;
+} */
+
+/* let index = 0;
+while (index < names.length) {
+    const element = names[index];
+    console.log(index, element);
+    index++;
+} */
+
+// do-while loop
+/* let i = 5;
+do {
+    console.log("value of i is :", i);
+    i++;
+} while (i < 8); */
+
+/* let index = 0;
+do {
+    const element = names[index];
+    console.log(index, element);
+    index++;
+} while (index < names.length); */
+
+// for-each loop
+// names.forEach((element, index) => { console.log(element, index) });
+/* function names(element, index) {
+    console.log(element, index);
+} */
+// names.forEach((element, index) => { console.log(element, index) });
+
+// for-of loop
+/* for (const [index, element] of names.entries()) {
+    console.log(index, element);
+} */
+
+// Default method
+/* for (const iterator of names) {
+    console.log(iterator);
+} */
+
+// for-in loop
+let obj = { a: 1, b: 2, c: 3 };
+for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        console.log(key, element);
+    }
+}
+for (const key in names) {
+    if (Object.hasOwnProperty.call(names, key)) {
+        const element = names[key];
+        console.log(key, element);
+    }
+}
