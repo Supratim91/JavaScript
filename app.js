1e99d84127082ff4cec0d72ea3e9fbc020510c33
Supratim Paul <supratim.paul0391@gmail.com>
2022-01-09 22:37:24 +0530

    Break & Continue Statement

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-const age = 25;
-if (age > 20) {
-    console.log("You are over 20 years old.");
-}
-else {
-    console.log("You are under 20 years old.");
-}
+// break & continue
+const scores = [50, 25, 0, 30, 100, 20, 10];
+for (let i = 0; i < scores.length; i++) {
 
+    if (scores[i] === 0) {
+        continue;
+    }
 
-const names = ['Rahul', 'Riya', 'Dev', 'Payal'];
-if (names.length > 3) {
-    console.log("Greater than 3");
-}
-else if (names.length < 2) {
-    console.log("Less than 2");
-}
-else {
-    console.log("Numbers of element in the array is : " + names.length);
-}
+    console.log('your score:', scores[i]);
 
-
-const pwd = "P@ssword";
-if (pwd.length >= 5 && pwd.indexOf('@') > -1) {
-    console.log("Password is strong");
-}
-else if (pwd.length >= 8) {
-    console.log("Password is long enough");
-}
-else {
-    console.log("Password is not long enough");
+    if (scores[i] === 100) {
+        console.log('congrats, you got the top score!');
+        break;
+    }
 }
