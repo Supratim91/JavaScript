81c759088c05571fc1c3e37be13ec6d98b2fd79e
Supratim Paul <supratim.paul0391@gmail.com>
2022-01-16 22:32:46 +0530

    Functions

--- app.js
+++ app.js
@@ -1,17 +1,112 @@
-let age = 30;           // global scope
+// function definition
+/* function greet(name) {
+    console.log("Hello World!", name);
+} */
 
-if (true) {
+const greet = (name) => { console.log("Hello World!", name); }        // arrow function
 
-    let age = 40;
-    let name = 'shaun';
-    console.log('inside 1st code block:', age, name);
+// function call
+greet("Rahul");        // entry point
+greet();
+greet('');
+console.log("================");
 
-    if (true) {
 
-        let age = 50;
-        console.log('inside 2nd code block:', age, name);
 
-    }
 
+// function definition 
+/* const speak = function (name = 'People', time = 'Day') {
+    console.log("Good", time + "!", name);
+} */
+
+const speak = (name = 'People', time = 'Day') => { console.log("Good", time + "!", name); }     // arrow function
+
+// function call
+speak("Shaun", "Night");        // entry point
+speak("Rajesh");
+speak(undefined, "Afternoon");
+speak(null, "Evening");
+speak();
+speak('');
+console.log("================");
+
+
+
+
+// bill([10, 15, 30], 0.2) using for-each loop
+/* function bill(products, tax) {
+    let total = 0;
+    products.forEach(element => {
+        total += element + (element * tax);
+    });
+    return total;
 }
-console.log('outside code block:', age, name);
+console.log("Total = ", bill([10, 15, 30], 0.2));
+console.log("================"); */
+
+
+// bill([10, 15, 30], 0.2) using for-of loop
+/* function bill(products, tax) {
+    let total = 0;
+    for (const element of products) {
+        total = (total + element) + (element * tax);
+    }
+    return total;
+}
+console.log("Total = ", bill([10, 15, 30], 0.2));
+console.log("================"); */
+
+
+// bill([10, 15, 30], 0.2) using for-in loop
+function bill(products, tax) {
+    let total = 0;
+    for (const iterator in products) {
+        if (Object.hasOwnProperty.call(products, iterator)) {
+            const element = products[iterator];
+            total += element + (element * tax);
+        }
+    }
+    return total;
+}
+console.log("Total = ", bill([10, 15, 30], 0.2));
+console.log("================");
+
+
+// convert this function bill([10, 15, 30], 0.2) using for-of loop to an arrow function
+/* const bill = (products, tax) => {
+    let total = 0;
+    for (const element of products) {
+        total = (total + element) + (element * tax);
+    }
+    return total;
+}
+console.log("Total = ", bill([10, 15, 30], 0.2));
+console.log("================"); */
+
+
+// convert this function bill([10, 15, 30], 0.2) using for-each loop to an arrow function
+/* const bill = (products, tax) => {
+    let total = 0;
+    products.forEach(element => {
+        total = (total + element) + (element * tax);
+    });
+    return total;
+}
+console.log("Total = ", bill([10, 15, 30], 0.2));
+console.log("================"); */
+
+
+
+
+/* Write a function to calculate area of a circle where radius is 5,
+also check the output of the function with radius 2.5
+Formulae: Area = PI * r ** 2
+Convert this function to an arrow function */
+function circle(area) {
+    let PI = 22 / 7, radius = 5;
+    let r = radius;
+    area = PI * r ** 2;
+    console.log("Area of the circle is = " + area);
+}
+circle();
+console.log("================");
