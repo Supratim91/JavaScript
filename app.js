3f865eb5c49cb95bf36b5fee2471564db45dcb2a
Supratim Paul <supratim.paul0391@gmail.com>
2022-01-23 23:43:52 +0530

    Object Literals

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-const people = ["Rahul", "Ravi", "Dev", "Sreelagna", "Shaon"];
-let html = "";
-let logPerson = (person, index) => {
-    html += `<li>${person}, ${index}</li>`;
-    console.log(`${person} is in ${index} position.`);
-}
-people.forEach(logPerson);
-const ul = document.querySelector(".people");       // query selector
-ul.innerHTML = html;
-// ul.innerText = html;
+let user = {
+    name: "Shaon",
+    age: 40,
+    email: "shaon01@gmail.com",
+    location: "Chennai",
+    blogs: [{ title: "Gardening Rules", likes: 30 }, { title: "Cooking Methods", likes: 20 }],
+    login: function () {
+        console.log("The user logged in.");
+    },
+    logout: function () {
+        console.log("The user logged out.");
+    },
+    logBlogs() {
+        console.log(`${this.name} has written following blogs:`);
+        this.blogs.forEach(blog => {
+            // console.log(blog);
+            console.log(`${blog.title} has ${blog.likes} likes.`);
+
+        });
+    }
+};
+console.log(user);
+console.log(user.age);
+console.log(user.name, user.blogs);
+user.age = 35;
+user["name"] = "Amit";
+console.log(user);
+console.log(typeof user);
+user.login();
+user.logout();
+user.logBlogs();
+console.log(this);
