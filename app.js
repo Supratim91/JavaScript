087b63a3d7a7aaadbcd9c50d32aeb2a036ef454d
Supratim Paul <supratim.paul0391@gmail.com>
2022-01-30 00:02:13 +0530

    Math Object

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
-let user = {
-    name: "Shaon",
-    age: 40,
-    email: "shaon01@gmail.com",
-    location: "Chennai",
-    blogs: [{ title: "Gardening Rules", likes: 30 }, { title: "Cooking Methods", likes: 20 }],
-    login: function () {
-        console.log("The user logged in.");
-    },
-    logout: function () {
-        console.log("The user logged out.");
-    },
-    logBlogs() {
-        console.log(`${this.name} has written following blogs:`);
-        this.blogs.forEach(blog => {
-            // console.log(blog);
-            console.log(`${blog.title} has ${blog.likes} likes.`);
-
-        });
-    }
-};
-console.log(user);
-console.log(user.age);
-console.log(user.name, user.blogs);
-user.age = 35;
-user["name"] = "Amit";
-console.log(user);
-console.log(typeof user);
-user.login();
-user.logout();
-user.logBlogs();
-console.log(this);
+// Math Object
+console.log(Math);
+console.log(Math.PI);
+const x = 55.63;
+console.log(Math.round(x));
+console.log(Math.floor(x));
+console.log(Math.ceil(x));
+console.log(Math.random());         // always returns within 0 to 1
+console.log(Math.trunc(x));
+console.log(Math.asinh(x));
