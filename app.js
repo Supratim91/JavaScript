690013a22627723c31365a15e68cefded75a8cb5
Supratim Paul <supratim.paul0391@gmail.com>
2022-01-30 00:33:28 +0530

    Primitive & Reference Value

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-// Math Object
-console.log(Math);
-console.log(Math.PI);
-const x = 55.63;
-console.log(Math.round(x));
-console.log(Math.floor(x));
-console.log(Math.ceil(x));
-console.log(Math.random());         // always returns within 0 to 1
-console.log(Math.trunc(x));
-console.log(Math.asinh(x));
+// primitive value
+const score1 = 50;
+const score2 = score1;
+console.log("Score 1 = " + score1 + ", " + "Score 2 = " + score2);
+
+// reference value
+let user1 = {
+    name: "Shubash",
+    age: 50
+};
+let user2 = user1;
+console.log("User 1 = ", user1, ", User 2 = ", user2);
+user1.name = "Ravi";
+console.log("User 1 = ", user1, ", User 2 = ", user2);
+let user3 = {};
+Object.assign(user3, user1);
+user1.age = 40;
+console.log("User 1 = ", user1, ", User 3 = ", user3);
