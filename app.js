bfc0e062bef0edf9249723881bc3008c64b25502
Supratim Paul <supratim.paul0391@gmail.com>
2022-02-26 23:40:07 +0530

    Sign-Up Form

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const btn = document.querySelector('button');
-btn.addEventListener('click', (ev) => {
-    console.log("Button is clicked");
-    console.log(ev.target.innerText);
-});
-
-const listItems = document.querySelectorAll('li');
-listItems.forEach(element => {
-    element.addEventListener('dblclick', (ev) => {
-        console.log("From lists clicked:" + ev.target.innerText);
-        console.log(element);
-        ev.target.innerText = "Gardening";
+const frm = document.getElementsByTagName('form');
+// const username = document.getElementById('username');
+Array.from(frm).forEach(form => {
+    form.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        // console.log("Form submitted: ", username.value);
+        console.log("Form submitted: ", form.username.value);
     });
 });
