a4995ff70d3bf90277a5ecbec58cd65f9eabb4b5
Supratim Paul <supratim.paul0391@gmail.com>
2022-02-28 12:21:48 +0530

    Bind-Call-Apply

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const frm = document.getElementsByTagName('form');
-// const username = document.getElementById('username');
-Array.from(frm).forEach(form => {
-    form.addEventListener('submit', (evt) => {
-        evt.preventDefault();
-        // console.log("Form submitted: ", username.value);
-        console.log("Form submitted: ", form.username.value);
-    });
-});
+const name = {
+    firstName: "John",
+    lastName: "Doe"
+};
+const name2 = {
+    firstName: "Sachin",
+    lastName: "Tendulkar"
+};
+printFullName = function (hometown, state) {
+    // console.log(this.firstName + " " + this.lastName);
+    console.log(`${this.firstName} ${this.lastName} lives in ${hometown}, ${state}`);
+}
+// name.printFullName();
+// printFullName.call(name2, "Mumbai", "India");        // context 1st, then other parameters
+// printFullName.call(name, "California", "US");
+
+// printFullName.apply(name, ["California", "US"]);        // context 1st, then 2nd argument array with parameters
+
+const printMyName = printFullName.bind(name2, "Mumbai", "India");       // context 1st, then other parameters
+printMyName();
