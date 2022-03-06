02bed2894dd4954c513f340fcc85f74030f0b5a2
Supratim Paul <supratim.paul0391@gmail.com>
2022-03-06 22:32:53 +0530

    Asynchronous Function All

--- app.js
+++ app.js
@@ -1,20 +1,93 @@
-const name = {
-    firstName: "John",
-    lastName: "Doe"
-};
-const name2 = {
-    firstName: "Sachin",
-    lastName: "Tendulkar"
-};
-printFullName = function (hometown, state) {
-    // console.log(this.firstName + " " + this.lastName);
-    console.log(`${this.firstName} ${this.lastName} lives in ${hometown}, ${state}`);
+// HTML button create "Track me", JS eventlistener add click
+// Asynchronous method handling using callback
+const button = document.querySelector('button');
+button.addEventListener('dblclick', trackUserHandler);
+function trackUserHandler() {
+    navigator.geolocation.getCurrentPosition((positionData) => {
+        console.log(positionData);
+    }, (error) => {
+        console.log(error);
+    });
 }
-// name.printFullName();
-// printFullName.call(name2, "Mumbai", "India");        // context 1st, then other parameters
-// printFullName.call(name, "California", "US");
 
-// printFullName.apply(name, ["California", "US"]);        // context 1st, then 2nd argument array with parameters
 
-const printMyName = printFullName.bind(name2, "Mumbai", "India");       // context 1st, then other parameters
-printMyName();
+// Asynchronous method handling using promise-then
+const getPosition = () => {
+    const promise = new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition((positionData) => {
+            resolve(positionData);
+            // reject("Error occurred");
+        }, (error) => {
+            // reject("Error occurred", error);
+        });
+    });
+    return promise;
+}
+function trackUserHandlerPromise() {
+    getPosition()
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        .finally(() => {
+            console.log("Finally called");
+        });
+}
+button.addEventListener('dblclick', trackUserHandlerPromise);
+
+
+// Asynchronous method handling using async/await
+async function trackUserHandlerAsync() {
+    try {
+        const data = await getPosition();
+        console.log(data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+button.addEventListener('dblclick', trackUserHandlerAsync);
+
+
+// Another example of promise-then
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        console.log(json);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+
+// Another example of above coding by using of async/await
+async function getTODOS() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await response.json();
+        console.log(data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+getTODOS();
+
+
+
+/* console.log("One");
+console.log("Two");
+// Asynchronous Function
+setTimeout(() => {
+    console.log("Callback fire");
+}, 0000);
+function handler() {
+    console.log("Callback fire");
+}
+setTimeout(handler, 2000);
+console.log("Three");
+console.log("Four"); */
